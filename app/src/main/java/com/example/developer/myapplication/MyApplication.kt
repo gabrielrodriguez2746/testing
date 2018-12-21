0e1c557adbc844b1f709c57317319c5936a81a5f
Gabriel Rodriguez <gabriel.rodriguez3103@gmail.com>
2018-12-21 09:30:29 -0500

    :star: Adding fabric configuration

--- app/src/main/java/com/example/developer/myapplication/MyApplication.kt
+++ app/src/main/java/com/example/developer/myapplication/MyApplication.kt
@@ -2,9 +2,12 @@ package com.example.developer.myapplication
 
 import android.app.Activity
 import android.app.Application
+import com.crashlytics.android.Crashlytics
+import com.crashlytics.android.core.CrashlyticsCore
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasActivityInjector
+import io.fabric.sdk.android.Fabric
 import javax.inject.Inject
 
 class MyApplication : Application(), HasActivityInjector {
@@ -17,10 +20,18 @@ class MyApplication : Application(), HasActivityInjector {
         DaggerAppComponent.builder()
                 .application(this).build()
                 .apply { inject(this@MyApplication) }
+        initFabric()
     }
 
     override fun activityInjector(): AndroidInjector<Activity> {
         return activityDispatchingAndroidInjector
+    }
+
+    private fun initFabric() {
+        val crashKit = Crashlytics.Builder()
+                .core(CrashlyticsCore.Builder().disabled(false).build()) // Fic this later
+                .build()
+        Fabric.with(this, crashKit)
     }
 
 }

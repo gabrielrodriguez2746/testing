7318fae449ceeb538d6db4e2d32640d2b6547521
Gabriel Rodriguez <gabriel.rodriguez3103@gmail.com>
2018-12-24 16:37:18 -0500

    :star: Configure bitrise

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./gradlew --no-daemon assembleDebug crashlyticsUploadDistributionDebug --build-cache -x lint -Dorg.gradle.jvmargs="-Xmx3g -XX:MaxPermSize=4g -XX:+HeapDumpOnOutOfMemoryError -Dfile.encoding=UTF-8"
+./gradlew --no-daemon assembleDebug --build-cache -x lint -Dorg.gradle.jvmargs="-Xmx3g -XX:MaxPermSize=4g -XX:+HeapDumpOnOutOfMemoryError -Dfile.encoding=UTF-8"
